2960009e46f7e24d0eb90a2abd81c264d201dc03
Paola <alejandra.medina.fernandez@gmail.com>
2025-10-12 22:38:51 -0300

    modificación index.js

--- JS/index.js
+++ JS/index.js
@@ -1,1 +1,59 @@
-"use string";
+"use strict";
+
+const tooltipsMap = {};
+const cerrarModal = document.getElementById('buut');
+
+function tooltipsWithEvents(selector, titleText, placement, forcePlacement, useBodyContainer) {
+    const elements = document.querySelectorAll(selector);
+
+    elements.forEach((element, index) => {
+        const key = `${selector}-${index}`; 
+
+        const options = {
+            title: titleText,
+            placement: placement
+        };
+
+        if (forcePlacement) {
+            options.fallbackPlacement = 'none';
+        }
+
+        element.addEventListener('mouseenter', () => {
+            if (!tooltipsMap[key]) {
+                element.removeAttribute('data-bs-toggle'); 
+                tooltipsMap[key] = new bootstrap.Tooltip(element, options);
+            }
+            tooltipsMap[key].show();
+        });
+
+        element.addEventListener('mouseleave', () => {
+            if (tooltipsMap[key]) {
+                tooltipsMap[key].hide();
+            }
+        });
+    });
+}
+// Tarjetas
+tooltipsWithEvents(
+    '.card-cover',
+    'País Árabe', 
+    'bottom', 
+    true
+);
+// Video
+tooltipsWithEvents(
+    '#vid', 
+    'Samia Gammal', 
+    'bottom',
+    true
+);
+// Página
+function verificarScroll(){
+    console.log("El usuario se desplazó por la página");
+}
+window.addEventListener("scroll",verificarScroll);
+// Cerrar
+function cerrar(){
+    console.log("El usuario cerró el modal");
+}
+cerrarModal.addEventListener("click", cerrar);
